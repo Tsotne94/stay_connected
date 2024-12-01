0aa1640e165b5c1f6bbbf3492e12f4f5a2877cf7
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-12-01 18:25:32 +0400

    feature: added no content image

--- QA_App/QA_App/MainAppModule/Scenes/HomePage/QusetionsTableView/QuestionsTableView.swift
+++ QA_App/QA_App/MainAppModule/Scenes/HomePage/QusetionsTableView/QuestionsTableView.swift
@@ -11,6 +11,11 @@ class QuestionsTableView: UIView {
     private let searchBar = UISearchBar()
     private let tagsCollection = TagsCollectionView()
     private let table = UITableView()
+    private let tableCount = 10
+    
+    private let noQuestionsLabel = UILabel()
+    private let beFirstLabel = UILabel()
+    private let backImage = UIImageView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,10 +27,18 @@ class QuestionsTableView: UIView {
     }
     
     private func setupView() {
-        setupSearchBar()
-        setupTags()
-        setupTableView()
-        setupConstraints()
+        if tableCount > 0 {
+            setupSearchBar()
+            setupTags()
+            setupTableView()
+            setupConstraints()
+        }
+        else {
+            setupFirstLabel()
+            setupSecondLabel()
+            setupBackgroundPicture()
+            setupEmpthyConstraints()
+        }
     }
     
     private func setupSearchBar() {
@@ -75,13 +88,55 @@ class QuestionsTableView: UIView {
             table.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
-
+    
+    private func setupFirstLabel() {
+        noQuestionsLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(noQuestionsLabel)
+        
+        noQuestionsLabel.text = "No questions yet"
+        noQuestionsLabel.textColor = UIColor(named: AppAssets.Colors.emptyStateText)
+        noQuestionsLabel.textAlignment = .center
+        noQuestionsLabel.font = .systemFont(ofSize: 15, weight: .medium)
+    }
+    
+    private func setupSecondLabel() {
+        beFirstLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(beFirstLabel)
+        
+        beFirstLabel.text = "Be the first to ask one"
+        beFirstLabel.textColor = UIColor(named: AppAssets.Colors.bodyText)
+        beFirstLabel.textAlignment = .center
+        beFirstLabel.font = .systemFont(ofSize: 15, weight: .medium)
+    }
+    
+    private func setupBackgroundPicture() {
+        backImage.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(backImage)
+        
+        backImage.image = UIImage(named: AppAssets.Icons.noContentBackground)
+        backImage.backgroundColor = .clear
+        backImage.contentMode = .scaleAspectFit
+        backImage.clipsToBounds = true
+    }
+    
+    private func setupEmpthyConstraints() {
+        NSLayoutConstraint.activate([
+            backImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -110),
+            backImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            beFirstLabel.bottomAnchor.constraint(equalTo: backImage.topAnchor, constant: -8),
+            beFirstLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            noQuestionsLabel.bottomAnchor.constraint(equalTo: beFirstLabel.topAnchor, constant: -8),
+            noQuestionsLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+        ])
+    }
 }
 
 extension QuestionsTableView: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        tableCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
