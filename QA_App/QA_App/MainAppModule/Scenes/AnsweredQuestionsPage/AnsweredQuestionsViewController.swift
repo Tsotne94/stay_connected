bba08cde6c553900a74cb58555ff6efaa03e8850
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2024-12-02 19:52:27 +0400

    feature: Answered Questions Page UI
    added answered questions page ui

---
+++ QA_App/QA_App/MainAppModule/Scenes/AnsweredQuestionsPage/AnsweredQuestionsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  AnsweredQuestionsViewController.swift
+//  QA_App
+//
+//  Created by Cotne Chubinidze on 02.12.24.
+//
+
+import UIKit
+
+class AnsweredQuestionsViewController: UIViewController {
+    private let backButton = UIButton()
+    private let titleLabel = UILabel()
+    private let questionTableView = QuestionsTableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        navigationController?.isNavigationBarHidden = true
+        view.backgroundColor = .white
+        setupBackButton()
+        setupTitleLabel()
+        setupTableView()
+        setupConstraints()
+    }
+    
+    private func setupBackButton() {
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backButton)
+        
+        backButton.backgroundColor = .clear
+        backButton.setTitle("", for: .normal)
+        backButton.setImage(UIImage(named: AppAssets.Icons.back), for: .normal)
+    }
+    
+    private func setupTitleLabel() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        titleLabel.font = UIFont(name: MyFonts.anekBold.rawValue, size: 20)
+        titleLabel.textAlignment = .left
+        titleLabel.textColor = UIColor(named: AppAssets.Colors.bodyText)
+        titleLabel.text = "Answered Questions"
+    }
+    
+    private func setupTableView() {
+        questionTableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(questionTableView)
+        
+        questionTableView.backgroundColor = .clear
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 14),
+            
+            titleLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 20),
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 14),
+            
+            questionTableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            questionTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            questionTableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            questionTableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+}
